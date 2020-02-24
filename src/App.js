faada781d3bfad8ab3d0447db6b1ed4a789b0f86
gannagoodkevich <gannagoodkevich@gmail.com>
2020-02-24 11:48:26 +0300

    Add App.js with 3 links: Home, Profile and Edit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* Import statements */
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+/* Home component */
+const Home = () => (
+    <div>
+        <h2>Welcome</h2>
     </div>
-  );
-}
+)
 
+const Profile = () => (
+    <div>
+        <h2>Profile</h2>
+    </div>
+)
+
+/* Products component */
+const Edit = () => (
+    <div>
+        <h2>Edit</h2>
+    </div>
+)
+
+/* App component */
+
+class App extends Component {
+    render() {
+
+        return (
+            <div>
+                <nav className="navbar navbar-light">
+                    <ul className="nav navbar-nav">
+                        <li><Link to="/">Homes</Link></li>
+                        <li><Link to="/profile">Category</Link></li>
+                        <li><Link to="/edit">Edit</Link></li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/edit" component={Edit}/>
+                </Switch>
+
+            </div>
+        );
+    }
+}
 export default App;
