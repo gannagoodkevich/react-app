dabdfcb3bb304702a6828127c206fe9bf27001ec
gannagoodkevich <gannagoodkevich@gmail.com>
2020-02-24 13:30:39 +0300

    Create localstorage values input and expenses to store them

--- src/Edit.js
+++ src/Edit.js
@@ -2,30 +2,61 @@ import React from 'react'
 class Edit extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            income: '',
+            expenses: ''};
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeIncome = this.handleChangeIncome.bind(this);
+        this.handleChangeExpenses = this.handleChangeExpenses.bind(this);
+        this.handleSubmitIncome = this.handleSubmitIncome.bind(this);
+        this.handleSubmitExpenses = this.handleSubmitExpenses.bind((this))
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
+    handleChangeIncome(event) {
+        this.setState({income: event.target.value});
     }
 
-    handleSubmit(event) {
-        localStorage.setItem('Bill', this.state.value);
+    handleChangeExpenses(event) {
+        this.setState({expenses: event.target.value});
+    }
+
+    handleSubmitIncome(event) {
+        let incomes = [];
+        if(localStorage.getItem("incomes") != null){
+         incomes = JSON.parse(localStorage.getItem("incomes"));
+        }
+        incomes.push(this.state.income);
+        localStorage.setItem("incomes", JSON.stringify(incomes));
+        event.preventDefault();
+    }
+
+    handleSubmitExpenses(event) {
+        let expenses = [];
+        if(localStorage.getItem("expenses") != null){
+            expenses = JSON.parse(localStorage.getItem("expenses"));
+        }
+        expenses.push(this.state.expenses);
+        localStorage.setItem("expenses", JSON.stringify(expenses));
         event.preventDefault();
     }
 
     render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
+        return (<div>
+            <form onSubmit={this.handleSubmitIncome}>
                 <label>
-                    Имя:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    Income
+                    <input type="text" onChange={this.handleChangeIncome} />
                 </label>
                 <input type="submit" value="Отправить" />
             </form>
+            <form onSubmit={this.handleSubmitExpenses}>
+                <label>
+                    Expenses
+                    <input type="text" onChange={this.handleChangeExpenses} />
+                </label>
+                <input type="submit" value="Отправить" />
+            </form>
+            </div>
         );
     }
 }
